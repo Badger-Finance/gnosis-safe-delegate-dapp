56ba507dbd74b6f86ce7c47f010d792e8937d9e7
sajanrajdev <sajanrajdev10@gmail.com>
2023-06-29 16:45:27 -0400

    feat: update safe api endpoints

--- src/utils.js
+++ src/utils.js
@@ -3,31 +3,31 @@ const {getAddress} = require("@ethersproject/address");
 const NETWORK = {
     "mainnet": {
         CHAINID: 1,
-        TX_SERVICE_BASE_URL: "https://safe-transaction.mainnet.gnosis.io",
+        TX_SERVICE_BASE_URL: "https://safe-transaction-mainnet.safe.global",
     },
     "rinkeby": {
         CHAINID: 4,
-        TX_SERVICE_BASE_URL: "https://safe-transaction.rinkeby.gnosis.io",
+        TX_SERVICE_BASE_URL: "https://safe-transaction-rinkeby.safe.global",
     },
     "goerli": {
         CHAINID: 5,
-        TX_SERVICE_BASE_URL: "https://safe-transaction.goerli.gnosis.io",
+        TX_SERVICE_BASE_URL: "https://safe-transaction-goerli.safe.global",
     },
     "xdai": {
         CHAINID: 100,
-        TX_SERVICE_BASE_URL: "https://safe-transaction.xdai.gnosis.io",
+        TX_SERVICE_BASE_URL: "https://safe-transaction-gnosis-chain.safe.global",
     },
     "matic": {
         CHAINID: 137,
-        TX_SERVICE_BASE_URL: "https://safe-transaction.polygon.gnosis.io",
+        TX_SERVICE_BASE_URL: "https://safe-transaction-polygon.safe.global",
     },
     "binance": {
         CHAINID: 56,
-        TX_SERVICE_BASE_URL: "https://safe-transaction.bsc.gnosis.io",
+        TX_SERVICE_BASE_URL: "https://safe-transaction-bsc.safe.global",
     },
     "arbitrum": {
         CHAINID: 42161,
-        TX_SERVICE_BASE_URL: "https://safe-transaction.arbitrum.gnosis.io",
+        TX_SERVICE_BASE_URL: "https://safe-transaction-arbitrum.safe.global",
     },
     "fantom": {
         CHAINID: 250,
@@ -36,7 +36,7 @@ const NETWORK = {
     },
     "optimism": {
         CHAINID: 10,
-        TX_SERVICE_BASE_URL: "https://safe-transaction.optimism.gnosis.io",
+        TX_SERVICE_BASE_URL: "https://safe-transaction-optimism.safe.global",
     }
 }
 
